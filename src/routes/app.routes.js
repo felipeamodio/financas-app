28d7f2da42cea40b6d282621b084c3c1a0318271
Felipe Alves <felipeaamodio@gmail.com>
2022-04-27 20:40:35 -0300

    fixed plugins and installation of drawer navigation

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,16 +1,16 @@
 // rotas quando estiver logado
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import Home from '../screens/Home';
 
-const AppStack = createNativeStackNavigator();
+const AppDrawer = createDrawerNavigator();
 
 function AppRoutes(){
     return(
-        <AppStack.Navigator>
-            <AppStack.Screen name="Home" component={Home} />
-        </AppStack.Navigator>
+        <AppDrawer.Navigator>
+            <AppDrawer.Screen name="Home" component={Home} />
+        </AppDrawer.Navigator>
     )
 }
 
