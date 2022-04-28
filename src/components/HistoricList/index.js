adc370185f06391caa171510e46f37cff92d3cab
Felipe Alves <felipeaamodio@gmail.com>
2022-04-28 20:47:34 -0300

    finish styling historic list component

--- src/components/HistoricList/index.js
+++ src/components/HistoricList/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import {
     Container,
-    Texto
+    Type,
+    IconView,
+    TextType,
+    Value
 } from './styles';
+
+import { Feather } from '@expo/vector-icons';
 
 export default function HistoricList(){
     return(
         <Container>
-            <Texto>historic list</Texto>
+            <Type>
+                <IconView>
+                    <Feather name="arrow-up" size={20} color="#FFFFFF" />
+                    <TextType>receita</TextType>
+                </IconView>
+            </Type>
+
+            <Value>R$900,00</Value>
         </Container>
     )
 }
--- src/components/HistoricList/styles.js
+++ src/components/HistoricList/styles.js
@@ -1,4 +1,37 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
-export const Texto = styled.Text``;
+export const Container = styled.View`
+    margin-bottom: 5px;
+    padding: 10px;
+    box-shadow: 2px 2px rgba(0, 0, 0, 0.40);
+    background-color: rgba(0, 0, 0, 0.02); //aumentar
+`;
+
+export const Type = styled.View`
+    flex-direction: row;
+`;
+
+export const IconView = styled.View`
+    flex-direction: row;
+    background-color: #049301;
+    padding-bottom: 3px;
+    padding-top: 3px;
+    padding-left: 8px;
+    padding-right: 8px;
+    border-radius: 7px;
+`;
+
+export const TextType = styled.Text`
+    color: #FFFFFF;
+    font-size: 16px;
+    font-style: italic;
+    font-weight: 600;
+    margin-left: 4px;
+`;
+
+export const Value = styled.Text`
+    color: #222222;
+    font-size: 22px;
+    font-weight: bold;
+    margin-top: 10px;
+`;
