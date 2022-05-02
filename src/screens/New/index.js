6afba9b778a72ebddd1643b9ae4c88d0dd26cd83
Felipe Alves <felipeaamodio@gmail.com>
2022-05-02 20:28:34 -0300

    created frontend of form to register financials

--- src/screens/New/index.js
+++ src/screens/New/index.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import { SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import {
+    Background,
+    Input,
+    SubmitButton,
+    SubmitText
+} from './styles';
+import DrawerButton from '../../components/DrawerButton';
 
 export default function New(){
+    const [value, setValue] = useState('');
+    const [type, setType] = useState(null);
+
     return(
-        <View>
-            <Text> NEW PAGE</Text>
-        </View>
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <Background>
+                <DrawerButton />
+
+                <SafeAreaView style={{alignItems: 'center'}}>
+                    <Input 
+                        placeholder="Valor desejado"
+                        keyboardType="numeric"
+                        returnKeyType="next"
+                        onSubmitEditing={() => Keyboard.dismiss()}
+                        value={value}
+                        onChangeText={(text) => setValue(text)}
+                    />
+
+                    <SubmitButton activeOpacity={0.7}>
+                        <SubmitText>Salvar</SubmitText>
+                    </SubmitButton>
+                </SafeAreaView>
+            </Background>
+        </TouchableWithoutFeedback>
     )
 }
